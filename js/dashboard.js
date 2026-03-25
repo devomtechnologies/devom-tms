e170fd83ec92c8b7dc2905d9a61b8f05d41055fb
devomtechnologies <devomtechnologies@gmail.com>
2026-03-25 17:29:24 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const userId = localStorage.getItem("currentUser");
+
+    if(!userId){
+        window.location.href = "login.html";
+        return;
+    }
+
+    const user = JSON.parse(localStorage.getItem(userId));
+
+    document.getElementById("welcome").innerHTML =
+        `Welcome ${user.name} (${user.transport})`;
+
+    // Demo Data
+    document.getElementById("trips").innerText = "12";
+    document.getElementById("balance").innerText = "₹ " + user.balance;
+    document.getElementById("pending").innerText = "₹ 5000";
+});
