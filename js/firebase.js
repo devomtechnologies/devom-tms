a401086ed219b0bda4aade91988478d1ae587838
devomtechnologies <devomtechnologies@gmail.com>
2026-04-02 15:40:14 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,15 +1,16 @@
+<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZNtuiJET6l0IfBaybKtsDkqvzW4qaDxg",
-  authDomain: "devom-tms.firebaseapp.com",
-  projectId: "devom-tms",
-  storageBucket: "devom-tms.firebasestorage.app",
-  messagingSenderId: "772473003335",
-  appId: "1:772473003335:web:c880a5dfb6aae18aaf4062",
-  measurementId: "G-WLL7YY5GCR"
+  apiKey: "AIzaSyCryX2UDfSn9vmYpT8jmgh-1abwEEF6KgY",
+  authDomain: "devom-tms-110f0.firebaseapp.com",
+  projectId: "devom-tms-110f0",
+  storageBucket: "devom-tms-110f0.firebasestorage.app",
+  messagingSenderId: "279439583320",
+  appId: "1:279439583320:web:dcccd1979b0c53f9cd822d",
+  measurementId: "G-8CHQLPL2E3"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
